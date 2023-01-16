d7da0781e77117c06b0c7aeaf41e429ff7d55e42
Simon Emms <simon@simonemms.com>
2023-01-16 20:18:32 +0000

    chore: done stuff

--- currency/currency.go
+++ currency/currency.go
@@ -2,13 +2,16 @@ package function
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"time"
 )
 
 const (
-	FeedURL = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml"
+	BaseCurrency = "EUR" // The base currency is "1"
+	FeedURL      = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml"
 )
 
 type ConversionRates struct {
@@ -18,23 +21,79 @@ type ConversionRates struct {
 	Currencies []Currency `xml:"Cube>Cube"`
 }
 
+func (c *ConversionRates) findCurrency() {
+
+}
+
+// Creates the conversion struct with the target data
+func (c *ConversionRates) Convert(from string, to string, amount float64, date *time.Time) (*Conversion, error) {
+	now := time.Now().UTC()
+	if date == nil {
+		date = &now
+	}
+
+	// Build the conversion struct
+	res := &Conversion{
+		// Set the datetime to be start of day
+		From: CurrencyValue{
+			Currency: from,
+			Value:    amount,
+		},
+		To: CurrencyValue{
+			Currency: to,
+		},
+	}
+
+	// Set the rates
+	if from == to {
+		// The currencies are the same - no conversion necessary
+		res.Rate = 1
+		// Date is unimportant, but stick to today for accuracy
+		date = &now
+	} else if from == BaseCurrency || to == BaseCurrency {
+		// Different currencies, but one is base currency
+		for _, k := range c.Currencies {
+			fmt.Println(k.Date)
+			fmt.Println(date)
+		}
+	}
+
+	fmt.Println(from)
+	fmt.Println(BaseCurrency)
+
+	os.Exit(1)
+
+	// Set to start of day
+	res.Date = date.Truncate(24 * time.Hour)
+
+	if err := res.Calculate(); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
 type Sender struct {
 	Name string `xml:"name"`
 }
 
 type Currency struct {
-	Date  string `xml:"time,attr"`
-	Rates []Rate `xml:"Cube"`
+	Date  CurrencyDate `xml:"time,attr"`
+	Rates []Rate       `xml:"Cube"`
 }
 
-// Convert perform the conversion based upon today's rate
-func (c *Currency) Convert(from string, to string, amount float64) (*Conversion, error) {
-	return c.ConvertDate(from, to, amount, time.Now())
+type CurrencyDate struct {
+	time.Time
 }
 
-// ConvertDate performs the conversion based upon the given day's rate
-func (c *Currency) ConvertDate(from string, to string, amount float64, date time.Time) (*Conversion, error) {
-	return nil, nil
+// Convert the string date into time.Time
+func (c *CurrencyDate) UnmarshalXMLAttr(attr xml.Attr) error {
+	parse, err := time.Parse("2006-01-02", attr.Value)
+	if err != nil {
+		return err
+	}
+	c.Time = parse
+	return nil
 }
 
 type Rate struct {
@@ -43,11 +102,20 @@ type Rate struct {
 }
 
 type Conversion struct {
-	Date  time.Time `json:"date"`
-	From  string    `json:"from"`
-	To    string    `json:"to"`
-	Rate  float64   `json:"rate"`
-	Value float64   `json:"value"`
+	Date time.Time     `json:"date"`
+	From CurrencyValue `json:"from"`
+	To   CurrencyValue `json:"to"`
+	Rate float64       `json:"rate"`
+}
+
+func (c *Conversion) Calculate() error {
+	c.To.Value = c.From.Value * c.Rate
+	return nil
+}
+
+type CurrencyValue struct {
+	Currency string  `json:"currency"`
+	Value    float64 `json:"value"`
 }
 
 func GetFromFeed(client http.Client) (c *ConversionRates, err error) {
@@ -68,9 +136,7 @@ func GetFromFeed(client http.Client) (c *ConversionRates, err error) {
 		return
 	}
 
-	c, err = ParseXML(data)
-
-	return
+	return ParseXML(data)
 }
 
 func ParseXML(data []byte) (*ConversionRates, error) {
--- currency/currency_test.go
+++ currency/currency_test.go
@@ -1,1 +1,120 @@
 package function_test
+
+import (
+	"encoding/json"
+	"flag"
+	"handler/function"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	update = flag.Bool("update", false, "update the golden files of this test")
+)
+
+const feedXML = "testdata/feed.xml"
+
+func TestParseXML(t *testing.T) {
+	goldenFile := "testdata/feed.json"
+
+	data, err := os.ReadFile(feedXML)
+	assert.Nil(t, err)
+
+	rates, err := function.ParseXML(data)
+	assert.Nil(t, err)
+
+	// Convert the feed to JSON format
+	got, err := json.MarshalIndent(rates, "", "  ")
+	require.NoError(t, err)
+
+	if *update {
+		err = os.WriteFile(goldenFile, []byte(got), 0600)
+		require.NoError(t, err)
+		return
+	}
+
+	content, err := os.ReadFile(goldenFile)
+	assert.Nil(t, err)
+
+	if diff := cmp.Diff(string(content), string(got)); diff != "" {
+		t.Errorf("non-matching golden file (-want +got):\n%s", diff)
+	}
+}
+
+func TestConvert(t *testing.T) {
+	data, err := os.ReadFile(feedXML)
+	require.NoError(t, err)
+
+	rates, err := function.ParseXML(data)
+	require.NoError(t, err)
+
+	testCases := []struct {
+		Name         string
+		From         string
+		To           string
+		Amount       float64
+		Date         *time.Time
+		ExpectedRate float64
+		Expected     float64
+		Currencies   []function.Currency
+	}{
+		// {
+		// 	Name:         "Same currency - whole number value",
+		// 	From:         "EUR",
+		// 	To:           "EUR",
+		// 	Amount:       26,
+		// 	Expected:     26,
+		// 	ExpectedRate: 1,
+		// },
+		// {
+		// 	Name:         "Same currency - decimal value",
+		// 	From:         "EUR",
+		// 	To:           "EUR",
+		// 	Amount:       10.56,
+		// 	Expected:     10.56,
+		// 	ExpectedRate: 1,
+		// },
+		{
+			Name:         "Based currency and non-base - whole number value",
+			From:         "GBR",
+			To:           "EUR",
+			Amount:       25,
+			Expected:     25,
+			ExpectedRate: 1,
+			Currencies:   rates.Currencies,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			if testCase.Currencies == nil {
+				testCase.Currencies = make([]function.Currency, 0)
+			}
+
+			c := function.ConversionRates{
+				Currencies: testCase.Currencies,
+			}
+
+			r, err := c.Convert(testCase.From, testCase.To, testCase.Amount, testCase.Date)
+			require.NoError(t, err)
+
+			assert.Equal(t, r, &function.Conversion{
+				Date: time.Now().UTC().Truncate(24 * time.Hour),
+				From: function.CurrencyValue{
+					Currency: testCase.From,
+					Value:    testCase.Amount,
+				},
+				To: function.CurrencyValue{
+					Currency: testCase.To,
+					Value:    testCase.Expected,
+				},
+				Rate: testCase.Expected,
+			})
+		})
+	}
+}
